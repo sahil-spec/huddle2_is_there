87bcb3037139f2c9a086ee12bd2dcabc88825048
Sahil Shukla <72448413+sahil-spec@users.noreply.github.com>
2023-01-05 11:29:43 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#oksignup').click(function(){
+        $('#login').addClass("reg");
+        $('#signup').removeClass("reg");
+    });
+    $('#oklogin').click(function(){
+        $('#signup').addClass("reg");
+        $('#login').removeClass("reg");
+    });
+  });
+  
+  
---
+++ usertable.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2022 at 05:19 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usertable`
+--
+
+CREATE TABLE `usertable` (
+  `Username` varchar(256) NOT NULL,
+  `Password` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `usertable`
+--
+
+INSERT INTO `usertable` (`Username`, `Password`) VALUES
+('SAHIL SHUKLA', 'SAHIL21'),
+('hehe', 'hehe');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
